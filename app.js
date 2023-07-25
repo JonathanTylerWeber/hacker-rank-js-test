f9b19b10a1708f3a15dcd3cc7f484ebc4c72af70
JonathanTylerWeber <jonathantweber@gmail.com>
2023-07-25 20:14:28 +0800

    3 done

--- app.js
+++ app.js
@@ -10,3 +10,16 @@ function removeFromString(str, index, count) {
     let modifiedStr = str.slice(0, index) + str.slice(index + count);
     return modifiedStr;
 }
+
+function reverse(arr) {
+    for (let i = 0; i < Math.floor(arr.length / 2); i++){
+        let x = arr[i];
+        arr[i] = arr[arr.length - 1 - i];
+        arr[arr.length - 1 - i] = x;
+    }
+    return arr
+}
+
+function min(arr) {
+
+}
